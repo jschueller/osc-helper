320e421dc04af4012e33199b1e90f475935d07b3
Julien Schueller <schueller@phimeca.com>
2016-06-10 18:37:31 +0200

    fix ttfautohint

--- repackage/ttfautohintx.sh
+++ repackage/ttfautohintx.sh
@@ -14,9 +14,23 @@ wget -c http://archive.ubuntu.com/ubuntu/pool/universe/t/ttfautohint/ttfautohint
 dpkg-source -x ttfautohint_0.97-1.dsc
 
 cd ttfautohint-0.97
-sed -i "s|--with autoreconf||g" debian/rules
+cat > debian/patches/fix-ftlib.patch <<EOF
+--- a/configure.ac      2016-06-10 16:12:21.000000000 +0000
++++ b/configure.ac      2016-06-10 16:15:40.805790084 +0000
+@@ -84,7 +84,7 @@
+ FREETYPE_LIBS="\`\$ft_config --libtool\`"
+ 
+ # many platforms no longer install .la files for system libraries
+-if test ! -f \$FREETYPE_LIBS; then
++if test ! -f "\$FREETYPE_LIBS"; then
+   FREETYPE_LIBS="\`\$ft_config --libs\`"
+ fi
 
-debuild -us -uc
+EOF
+echo "fix-ftlib.patch" >> debian/patches/series
+
+
+# debuild -us -uc
 debuild -us -uc -S
 cd ..
 cp ttfautohint_0.97-1.dsc ttfautohint-xUbuntu_16.04.dsc
